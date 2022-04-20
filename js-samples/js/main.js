a928f11873c913e7e9f8cc6938c78092b5cd0f44
jongeun77 <69672989+jongeun77@users.noreply.github.com>
2022-04-20 17:18:33 +0900

    feature3 added

--- js-samples/js/main.js
+++ js-samples/js/main.js
@@ -1,5 +1,9 @@
+
+
+
 // Heder scroll
 $(window).scroll(function(){
+
     if($(window).scrollTop()>50){
         $('header').addClass('active')
     }
@@ -25,7 +29,7 @@ $(window).scroll(function(){
     if($(window).scrollTop()>50){
         $('.go_btn').addClass('active')
     }
-    else(
+    else(  
         $('.go_btn').removeClass('active')
         )
     })   
@@ -40,8 +44,63 @@ $(function(){
       });
     })
 
+//tab1 : tab a에 data-alt, list에 id 네임 일치해서 불러오는법
+/*
+$(function(){
+    $('.tab_btn').click(function(){
+       
+      $(this).addClass('active');
+      $(this).siblings().removeClass('active');
+
+       $('.list').removeClass('active');
+      $('#' + $(this).attr('data-alt')).addClass('active');
+    })
+  
+  })
+*/
+
+//tab2 : eq(idx), idx=$(this).index() 로 불러오는법
+/*
+$(function(){
+    var tab_btn =$('.tab_btn')
+    var list =$('.list')
+   
+    tab_btn.eq(0).addClass('active')//처음 tab에 active
+    list.eq(0).addClass('active')//처음 list에 active
+
+    tab_btn.click(function(){
+        var idx =$(this).index()//tab의 갯수 변수화
+
+        $(this).siblings().removeClass('active')//tab siblings에 디자인 active뺴기
+        $(this).addClass('active')//tab this에  active 넣기
+
+        list.removeClass('active')//list에 active 빼기
+        list.eq(idx).addClass('active')//list에 tab의 갯수만큼의 변수idx 만큼 active
+    })
+
+})
+*/
+//tab3 : 함수로 불러오기 tab.click(함수) , function 함수(){}
+$(function(){
+    const tab_btn =$('.tab_btn')
+    const list =$('.list')
+    tab_btn.click(tab)
+    
+    tab_btn.eq(0).addClass('active')//처음 tab에 active
+    list.eq(0).addClass('active')//처음 list에 active
+    
+    function tab(){
+        const idx = $(this).index()//tab의 갯수 변수화 (함수내에 작성)
 
 
+        $(this).siblings().removeClass('active')//tab siblings에 디자인 active뺴기
+        $(this).addClass('active')//tab this에  active 넣기
 
+        list.removeClass('active')//list에 active 빼기
+        list.eq(idx).addClass('active')//list에 tab의 갯수만큼의 변수idx 만큼 active
+    }
+        
+   
 
+})
 
